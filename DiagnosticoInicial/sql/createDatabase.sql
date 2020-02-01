6fea07f5dcbefc0f464b7b5e0b039778bc7244e7
Erick-ECE <ericken15@ciencias.unam.mx>
2020-01-31 23:44:52 -0600

    DB

---
+++ DiagnosticoInicial/sql/createDatabase.sql
@@ -0,0 +1,41 @@
+ -- create the database
+CREATE DATABASE conductores; 
+
+ -- create tables
+CREATE TABLE chofer(
+	id_chofer SERIAL PRIMARY KEY,
+    nombre_pila VARCHAR(25),
+    apellido_paterno VARCHAR(25),
+	apellido_materno VARCHAR(25),
+	calle VARCHAR(25),
+	correo_electronico VARCHAR(320),
+	fecha_nacimiento DATE,
+	fecha_ingreso DATE,    
+);
+
+CREATE TABLE carros(
+	placas CHAR(7) PRIMARY KEY,
+	marca VARCHAR(15),
+	submarca VARCHAR(15),
+	año CHAR(4),
+	color VARCHAR(22),
+	capacidad SMALLINT,
+	precio_factura NUMERIC(6,2)
+);
+
+CREATE TABLE telefonos(
+	id_chofer INT REFERENCES chofer(id_chofer),
+	telefonos VARCHAR(10)
+);
+
+
+CREATE TABLE maneja(
+	id_chofer INT REFERENCES chofer(id_chofer),
+	placas_carro CHAR(7) REFERENCES carros(placas),
+	fecha_inicio DATE,
+	fecha_fin DATE
+);
+
+
+		
+
