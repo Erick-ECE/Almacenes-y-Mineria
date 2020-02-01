af0c6dea23ea5a80cbe5df2d78ed0579f134cd10
Erick-ECE <ericken15@ciencias.unam.mx>
2020-02-01 00:29:04 -0600

    querys

---
+++ DiagnosticoInicial/sql/querys.sql
@@ -0,0 +1,25 @@
+-- -----QUERYS----- --
+
+-- 1.- Conocer los datos de los automóviles que tengan más de 15 años de haberse comprado.
+   
+    --pendiente
+
+-- 2.- Conocer los datos de los automóviles que están contenidos en cada una de
+    -- las categorías (Lite, Luxury). No olvides que la categoría se debe visualizar
+    -- dentro de los datos del automóvil.
+
+    --pendiente
+
+-- 3.- Conocer el nombre, edad y la fecha en la que inició a trabajar de todos los choferes.
+
+    --pendiente
+
+-- 4.- Conocer el nombre y edad de todos los choferes que han conducido en más
+    -- de un automóvil.
+
+    --pendiente
+
+-- 5.- Conocer la capacidad de pasajeros, el costo, categoría y modelo de cada
+    -- uno de los automóviles.
+
+    --pendiente
